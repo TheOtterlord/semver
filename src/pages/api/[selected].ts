5bd32536dc90c3820dd2663f3d759db2d51b6845
Arsh <69170106+lilnasy@users.noreply.github.com>
2023-11-17 21:00:01 +0000

    fix: scoped packages

--- src/pages/api/[selected].ts
+++ src/pages/api/[selected].ts
@@ -1,12 +1,18 @@
 import type { APIRoute } from "astro"
 
 export const get: APIRoute = async ({ params }) => {
-  const res = await fetch(`https://registry.npmjs.org/${params.selected}`) //
-  return {
-    body: await res.text(),
+  const { selected } = params 
+  
+  if (selected === undefined) return new Response("Bad Request", { status: 400 })
+  
+  const res = await fetch(`https://registry.npmjs.org/${atob(selected)}`)
+  
+  if (res.ok === false) return new Response("Not Found", { status: 404 })
+  
+  return new Response(res.body, {
     headers: {
       "content-type": "application/json",
       "cache-control": "max-age=3600"
     }
-  }
+  })
 }
