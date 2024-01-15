db21c9a76acd7dbc15baf87343c55415a1a4239b
= <otterlord.dev@gmail.com>
2024-01-15 21:56:10 +0000

    Fix adapter, rename file to match astro regex & clean up older a11y issues

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,5 @@ import cloudflare from "@astrojs/cloudflare";
 export default defineConfig({
   integrations: [tailwind()],
   output: 'server',
-  adapter: cloudflare()
+  adapter: cloudflare({ imageService: 'passthrough' })
 });
